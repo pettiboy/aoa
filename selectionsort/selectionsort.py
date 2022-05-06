e7b57de064dc1c508fbdb37da8d8ce559fb2bc24
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-05-06 16:05:31 +0530

    refactor: selectionsort

--- selectionsort/selectionsort.py
+++ selectionsort/selectionsort.py
@@ -3,15 +3,17 @@ from timeit import default_timer as timer
 from datetime import timedelta
 
 
-def selectionSort(array):
-    for step in range(len(array)):
-        minIndex = step
-        for i in range(step + 1, len(array)):
-            # select the minimum element in each loop
-            if array[i] < array[minIndex]:
-                minIndex = i
-        # put min at the correct position
-        array[step], array[minIndex] = array[minIndex], array[step]
+def selectionSort(array, n):
+    for i in range(n):
+
+        # find min element ka index
+        minIndex = i
+        for j in range(i + 1, n):
+            if array[j] < array[minIndex]:
+                minIndex = j
+
+        # swap min ele with the step being looped over
+        array[i], array[minIndex] = array[minIndex], array[i]
 
 
 def main():
@@ -21,11 +23,12 @@ def main():
     data = [random.randrange(0, numOfElements * 10)
             for _ in range(numOfElements)]
 
+    print("Before: ", data)
     start = timer()
-    selectionSort(data)
+    selectionSort(data, len(data))
     end = timer()
+    print("After: ", data)
 
-    # print("Sorted:", data)
     print("Time:", timedelta(seconds=end - start))
 
 
