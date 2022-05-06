18fcb1838f35ca160eeba3cb7957a310a151b05f
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-05-06 14:26:09 +0530

    refactor: insertion sort

--- insersionsort/insertionsort.py
+++ insersionsort/insertionsort.py
@@ -4,14 +4,18 @@ from datetime import timedelta
 
 
 def insertionSort(array):
-    for step in range(1, len(array)):
-        key = array[step]
-        j = step - 1
-        # Compare key with each element on the left of it until an element smaller than it is found
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+
+        # Compare key with each element on the left of it
+        # until an element smaller than it is found
         while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
             j = j - 1
-        # Place key at after the element just smaller than it.
+
+        # Place key at after the element
+        # just smaller than it.
         array[j + 1] = key
 
 
@@ -19,13 +23,16 @@ def main():
     print("Length: ", end="")
     numOfElements = int(input())
 
-    data = [random.randrange(0, numOfElements * 10) for _ in range(numOfElements)]
+    data = [random.randrange(0, numOfElements * 10)
+            for _ in range(numOfElements)]
+
+    print("un sorted:", data)
 
     start = timer()
     insertionSort(data)
     end = timer()
 
-    # print("Sorted:", data)
+    print("Sorted:", data)
     print("Time:", timedelta(seconds=end - start))
 
 
