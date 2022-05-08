513db7ecb13f43f78e9130cf731eddcddb4a8eb5
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-05-08 18:50:40 +0530

    fix: python knapsack

--- python/knapsack/fractional/knapsack.py
+++ python/knapsack/fractional/knapsack.py
@@ -3,7 +3,7 @@ class ItemValue:
         self.index = index
         self.profit = profit
         self.weight = weight
-        self.cost = profit // weight
+        self.cost = profit / weight
 
     def __lt__(self, other):
         return self.cost < other.cost
@@ -12,23 +12,24 @@ class ItemValue:
 def calculate_max_value(weights, values, capacity):
     items = []
     for i in range(len(weights)):
-        items.append(ItemValue(weights[i], values[i], i))
+        items.append(ItemValue(values[i], weights[i], i))
 
     # sorting items by value
     items.sort(reverse=True)
 
     total_value = 0
     for i in items:
-        current_weight = int(i.weight)
-        current_profit = int(i.profit)
+
+        current_weight = i.weight
+        current_profit = i.profit
 
         if capacity - current_weight >= 0:
             capacity -= current_weight
             total_value += current_profit
         else:
             fraction = capacity / current_weight
+            capacity -= current_weight * fraction
             total_value += current_profit * fraction
-            capacity = int(capacity - (current_weight * fraction))
             break
 
     return total_value
