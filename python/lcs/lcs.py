da79fc57a6c63f9c4f3dac1adb483687fba16673
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-05-10 19:24:38 +0530

    refactor: python lcs

--- python/lcs/lcs.py
+++ python/lcs/lcs.py
@@ -1,37 +1,36 @@
-def lcs_algo(S1, S2, m, n):
-    L = [[0 for x in range(n+1)] for x in range(m+1)]
+def lcs_algo(s1, s2, m, n):
+    mat = [[0 for _ in range(n+1)] for _ in range(m+1)]
 
     # Building the mtrix in bottom-up way
     for i in range(m+1):
         for j in range(n+1):
             if i == 0 or j == 0:
-                L[i][j] = 0
-            elif S1[i-1] == S2[j-1]:
-                L[i][j] = L[i-1][j-1] + 1
+                mat[i][j] = 0
+            elif s1[i-1] == s2[j-1]:
+                mat[i][j] = mat[i-1][j-1] + 1
             else:
-                L[i][j] = max(L[i-1][j], L[i][j-1])
+                mat[i][j] = max(mat[i-1][j], mat[i][j-1])
 
-    index = L[m][n]
+    index = mat[m][n]
 
-    lcs_algo = [""] * (index+1)
-    lcs_algo[index] = ""
+    result = [""] * (index+1)
 
     i = m
     j = n
     while i > 0 and j > 0:
 
-        if S1[i - 1] == S2[j - 1]:
-            lcs_algo[index - 1] = S1[i - 1]
+        if s1[i - 1] == s2[j - 1]:
+            result[index - 1] = s1[i - 1]
             i -= 1
             j -= 1
             index -= 1
 
-        elif L[i - 1][j] > L[i][j - 1]:
+        elif mat[i - 1][j] > mat[i][j - 1]:
             i -= 1
         else:
             j -= 1
 
-    print("LCS: " + "".join(lcs_algo))
+    print("LCS: " + "".join(result))
 
 
 def main():
